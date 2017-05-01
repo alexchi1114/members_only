bb72faf8f2e098a994f7aab7c75a8d7a9c903bed
AlexChi <alexchi1114@gmail.com>
2017-05-01 19:36:52 -0400

    Started work on logging in with sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
 	def new
 	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		#If the user exists and the password is correct, 
+		#log the user in and redirect to user page.
+		if user && user.authenticate(params[:session][:password])
+			redirect_to user
+		else
+			flash.now[:danger] = "Invald email and/or password"
+			render 'new'
+		end
+	end
+
+	def destroy
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
 	get '/login', to: 'sessions#new'
 
+	post '/login', to: 'sessions#create'
+
+	delete '/logout', to: 'sessions#destroy'
+
 	root 'static_pages#home'
 
 	resources :users
