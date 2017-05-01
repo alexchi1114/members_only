b152152fc96704bd4e7139e49a5372275d93fa22
AlexChi <alexchi1114@gmail.com>
2017-05-01 19:01:40 -0400

    Finished sign up and created sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
 	get '/signup', to: 'users#new'
 
+	get '/contact', to: 'static_pages#contact'
+
+	get '/about', to: 'static_pages#about'
+
+	get '/login', to: 'sessions#new'
+
 	root 'static_pages#home'
 
 	resources :users
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+	   	@base_title = "Members Only"
+	   	@user = users()
+	end
+
+  	test "should get new" do
+  		get signup_path
+  		assert_response :success
+  		assert_select "title", "Sign up | #{@base_title}"
+ 	end
+
+ 	test "should get index" do
+ 		get users_path
+ 		assert_response :success
+ 		assert_select "title", "Users | #{@base_title}"
+ 	end
+
 end
